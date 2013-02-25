75264e3a07881f729307912850ec293686c0746a
Damir Foy <damirfoy@gmail.com>
2013-02-26 01:17:51 +0400

    release

--- README.md
+++ README.md
@@ -1,4 +1,119 @@
-icheck
-======
+# iCheck
 
-Highly customizable jQuery plugin for checkboxes and radio buttons custom styling
+#### A lightweight jQuery plugin for checkboxes and radio buttons custom styling
+#### [Demo](http://damirfoy.com/projects/icheck/)
+
+## Features:
+
+* **Highly customizable**
+* Focused on performance
+* Runs on modern and ancient browsers, including mobile
+* Touch devices support
+* jQuery version 1.6 and newer support
+* **5 Retina-ready skins** — `/skins/` folder
+* Lightweight size — core is 1.1 kb gzipped
+
+---
+
+* Keeps original inputs in your code
+* Works with checkboxes and radio buttons only, but handles any jQuery selectors
+* In friendship with your CSS reset
+* Keyboard shortcuts support (e.g. `Tab`, `Spacebar`, `Arrow up/down`)
+* jQuery chaining support on any action
+* Ability to change plugin options on selected inputs after they are customized
+* Styling ajax loaded inputs
+
+---
+
+* Separate checkboxes and radio buttons class names
+* State classes: `.checked`, `.disabled`, `.focus` and `.active` (`:hover` is native)
+* Option to append custom HTML code or text inside customized inputs
+* Global and per element **callbacks** when input is `created`, `clicked`, `checked` and `unchecked`
+* **Methods** to add or remove `checked` state on selected inputs programatically
+* Option to handle only checkboxes or radio buttons, both by default
+* Adds hand cursor over input if you set up
+* Option to inherit original input's class name or id
+* Increasing or decreasing clickable area around input
+
+## Options:
+
+    {
+        handle: '', // 'checkbox' or 'radio' to style only checkboxes or radio buttons, both by default
+        checkboxClass: 'icheckbox', // checkboxes class name
+        radioClass: 'iradio', // radio buttons class
+        checkedClass: 'checked', // checked state class
+        disabledClass: 'disabled', // disabled state class
+        focusClass: 'focus', // focus state class
+        increaseArea: '', // increase clickable area by given % (negative to decrease)
+        cursor: false, // true to set hand cursor over input
+        inheritClass: false, // set true to inherit input's class name
+        inheritID: false, // if set to true, input's id is prefixed with 'icheck-' and attached
+        insert: '' // add custom HTML code or text inside customized input
+    }
+
+Note: you can choose any class names and slyle them as you want.
+
+## Usage
+
+iCheck supports direct and parent selectors, handles only checkboxes and radio buttons:
+
+    $('body').icheck(); // customize all inputs on the page
+    $('.block input').icheck(); // styling inputs only inside $('.block')
+    $('input.vote').icheck(); // styling all inputs with the "vote" class
+
+Example:
+
+    $(document).ready(function(){
+        $('input').icheck({
+            // options
+        });
+
+        // you can also change options after inputs are customized
+        $('input.some').icheck({
+            // different options (skin e.g.)
+        });
+    });
+
+Note: don't forget to include jQuery (version 1.6 or newer) and jquery.icheck.js (or jquery.icheck.min.js) in your HTML.
+
+## Callbacks
+
+`this.clicked` event fires when user clicks on customized input (not used when you change it's state programatically)
+`this.checked` fires when input goes to `checked` state
+`this.unchecked` fires when `checked` state is removed
+`this.created` fires when input is created
+
+Use `bind` (also `live/delegate`) or `on` (modern jQuery versions) to attach them:
+
+    $('input').bind('this.clicked', function(){
+        console.log('input is clicked');
+    });
+
+Note: `this.created` callback should be binded before plugin init:
+
+    $('input').bind('this.created', function(){
+            console.log('input is created');
+    }).icheck({
+            // options
+    });
+
+## Methods
+
+    $('input').icheck('check'); // change input's state to 'checked'
+    $('input').icheck('uncheck'); // remove 'checked' state
+    $('input').icheck('update'); // apply input changes, which were done outside the plugin
+
+## Browser support:
+
+* Internet Explorer 7+ (works in IE6 if you don't use CSS class chaining)
+* Firefox 2+
+* Opera 9+
+* Google Chrome
+* Safari
+* others
+
+Tested on mobile devices.
+
+## License
+
+iCheck is released under [MIT License](http://en.wikipedia.org/wiki/MIT_License), you are allowed to use it anywhere you want for free.
---
+++ js/jquery.icheck.js
@@ -0,0 +1,224 @@
+/*
+    iCheck v1.0
+    ===========
+    http://github.com/damirfoy/icheck
+    ---------------------------------
+    Highly customizable jQuery plugin for checkboxes and radio buttons custom styling
+
+    Options:
+    -------
+    {
+        handle: '', // 'checkbox' or 'radio' to style only checkboxes or radio buttons, both by default
+        checkboxClass: 'icheckbox', // checkboxes class name
+        radioClass: 'iradio', // radio buttons class
+        checkedClass: 'checked', // checked state class
+        disabledClass: 'disabled', // disabled state class
+        focusClass: 'focus', // focus state class
+        increaseArea: '', // increase clickable area by given % (negative to decrease)
+        cursor: false, // true to set hand cursor over input
+        inheritClass: false, // set true to inherit input's class name
+        inheritID: false, // if set to true, input's id is prefixed with 'icheck-' and attached
+        insert: '' // add custom HTML code or text inside customized input
+    }
+
+        Note: you can choose any class names and slyle them as you want.
+
+    Usage:
+    -----
+        iCheck supports direct and parent selectors, handles only checkboxes and radio buttons:
+
+        $('body').icheck(); // customize all inputs on the page
+        $('.block input').icheck(); // styling inputs only inside $('.block')
+        $('input.vote').icheck(); // styling all inputs with the "vote" class
+
+        Example:
+
+        $(document).ready(function(){
+            $('input').icheck({
+                // options
+            });
+
+            // you can also change options after inputs are customized
+            $('input.some').icheck({
+                // different options (skin e.g.)
+            });
+        });
+
+        Note: don't forget to include jQuery (version 1.6 or newer) and jquery.icheck.js (or jquery.icheck.min.js) in your HTML.
+
+    Callbacks:
+    ---------
+        'this.clicked' event fires when user clicks on customized input (not used when you change it's state programatically)
+        'this.checked' fires when input goes to 'checked' state
+        'this.unchecked' fires when 'checked' state is removed
+        'this.created' fires when input is created
+
+        Use 'bind' (also 'live/delegate') or 'on' (modern jQuery versions) to attach them:
+
+        $('input').bind('this.clicked', function(){
+            console.log('input is clicked');
+        });
+
+        Note: 'this.created' callback should be binded before plugin init:
+
+        $('input').bind('this.created', function(){
+            console.log('input is created');
+        }).icheck({
+            // options
+        });
+
+    Methods:
+    -------
+    $('input').icheck('check'); // change input's state to 'checked'
+    $('input').icheck('uncheck'); // remove 'checked' state
+    $('input').icheck('update'); // apply input changes, which were done outside the plugin
+
+    Browser support:
+    ---------------
+        Internet Explorer 7+ (works in IE6 if you don't use CSS class chaining)
+        Firefox 2+
+        Opera 9+
+        Google Chrome
+        Safari
+        others
+
+        Tested on mobile devices
+
+    =========================
+    Copyright 2013, Damir Foy
+    Released under MIT License
+*/
+
+(function($){
+    var settings,
+        methods = {
+            init: function(options){
+                var defaults = {checkboxClass: 'icheckbox', radioClass: 'iradio', checkedClass: 'checked', disabledClass: 'disabled', focusClass: 'focus', activeClass: 'active'};
+                settings = $.extend({}, defaults, options);
+                var filter = (/^(checkbox|radio)$/i).test(settings.handle) ? ':' + settings.handle.toLowerCase() : ':checkbox, :radio',
+                    area = (get_number(settings.increaseArea)) ? get_number(settings.increaseArea) : 0;
+
+                area && (area < 51 && settings.increaseArea.toString().charAt(0) == '-') && (area = '-' + area);
+
+                return this.each(function(){
+                    var elements = $(this).is(filter) ? $(this) : $(this).find(filter);
+
+                    elements.each(function(){
+                        var self = $(this);
+
+                        self.data('state') && (self.parent().html(self), self.unwrap());
+
+                        var className = (this.type == 'checkbox') ? settings.checkboxClass : settings.radioClass,
+                            parent = self.css({
+                                position: 'absolute',
+                                top: -area + '%',
+                                left: -area + '%',
+                                width: 100 + (area * 2) + '%',
+                                height: 100 + (area * 2) + '%',
+                                margin: 0,
+                                padding: 0,
+                                border: 0,
+                                opacity: 0
+                            }).wrap('<div class="' + className + '"/>').parent().prepend(settings.insert);
+
+                        settings.cursor == true && !this.disabled && self.css('cursor', 'pointer');
+                        settings.inheritClass == true && parent.addClass(this.className);
+                        settings.inheritID == true && this.id && parent.attr('id', 'icheck-' + this.id);
+                        parent.css('position') == 'static' && parent.css('position', 'relative');
+
+                        this.checked ? parent.data('checked', true).addClass(settings.checkedClass) : parent.data('checked', false);
+                        this.disabled && parent.addClass(settings.disabledClass);
+
+                        self.data('state', {
+                            checked: settings.checkedClass,
+                            focus: settings.focusClass,
+                            active: settings.activeClass
+                        }).trigger('this.created').click(change).bind('focus blur mousedown mouseout touchbegin touchend', function(event){
+                            var states = get_state(self, false), state, type = event.type;
+
+                            type == 'focus' && (state = states.focus);
+                            type == 'blur' && (state = states.focus + ' ' + states.active);
+
+                            (/^mousedown|mouseout|touchbegin|touchend$/i).test(type) && (state = states.active);
+                            (/^blur|mouseout|touchend$/i).test(type) ? parent.removeClass(state) : parent.addClass(state);
+                        });
+                    });
+                });
+            },
+            check: function(){
+                $(this).each(function(){
+                    change('check', $(this));
+                });
+            },
+            uncheck: function(){
+                $(this).each(function(){
+                    change('uncheck', $(this));
+                });
+            },
+            update: function(){
+                $(this).each(function(){
+                    change('update', $(this));
+                });
+            }
+        };
+
+    $.fn.icheck = function(method){
+        if (navigator.userAgent.indexOf('Opera Mini') > -1)
+            return this;
+        else if (methods[method])
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        else if (typeof method == 'object' || !method)
+            return methods.init.apply(this, arguments);
+    };
+
+    function change(state, input){
+        var direct = 'object' == typeof input,
+            input = direct ? input : $(this),
+            checked = input[0].checked;
+
+        if (state == 'check' && !checked)
+            check(input, true);
+        else if (state == 'uncheck' && checked)
+            uncheck(input, true);
+        else if (state == 'update' || !direct){
+            !direct && input.trigger('this.clicked');
+            checked ? check(input) : uncheck(input);
+        }
+    }
+
+    function check(input, state){
+        var parent = input.parent();
+
+        state && (input[0].checked = true);
+
+        if (parent.data('checked') !== true){
+            if (input[0].type == 'radio'){
+                $('input[name=' + input[0].name + ']').each(function(){
+                    this !== input[0] && uncheck($(this));
+                });
+            }
+
+            input.trigger('this.checked');
+            parent.data('checked', true).addClass(get_state(input, 'checked'));
+        }
+    }
+
+    function uncheck(input, state){
+        var parent = input.parent();
+
+        state && (input[0].checked = false);
+
+        if (parent.data('checked') !== false){
+            input.trigger('this.unchecked');
+            parent.data('checked', false).removeClass(get_state(input, 'checked'));
+        }
+    }
+
+    function get_state(input, state){
+        return state == false ? input.data('state') : input.data('state')[state];
+    }
+
+    function get_number(value){
+        return Math.abs(parseInt(value, 10));
+    }
+})(jQuery);
